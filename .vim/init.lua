3aa46c7ce8a994c1c96d2cfb966f5b93d27623a5
Brandon Mittman <BrandonMittman@gmail.com>
2023-04-14 21:10:56 -0500

    Update colorscheme setup

--- .vim/init.lua
+++ .vim/init.lua
@@ -72,9 +72,10 @@ require('packer').startup(function(use)
     use {'chriskempson/base16-vim', config =
     vim.api.nvim_exec(
         [[
-            if filereadable(expand("~/.vimrc_background"))
+            if exists('$BASE16_THEME')
+            \ && (!exists('g:colors_name') || g:colors_name != 'base16-$BASE16_THEME')
                 let base16colorspace=256
-                source ~/.vimrc_background
+                colorscheme base16-$BASE16_THEME
             endif
         ]],
         true)
